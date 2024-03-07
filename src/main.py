d34ed4e194f21c2ea70058fb2b3e70f1df9b4d61
Miguel Ramos <return.infox@gmail.com>
2024-03-06 21:10:32 -0300

    codigo finalizado

--- src/main.py
+++ src/main.py
@@ -5,6 +5,21 @@ def print_menu():
     print("2. Listar todos los participantes")
     print("3. Salir")
 
+def assign_email(registration):
+    print(f"\n Asignando un correo para: {registration['name']} ({registration['role']})")
+    while True:
+        email = input("Introduce el electrónico: ")
+        if validate_email(email):
+            registration['email'] = email
+            print(f"Registro actualizado: {registration}")
+            break
+        else:
+            print("El correo electronico es invalido. ")
+
+def list_participants(participants):
+    for registration in participants:
+        print(registration)
+
 def main():
     participants = [
         create_registration("Catalina", "Estudiante"),
@@ -12,6 +27,22 @@ def main():
         create_registration("Elon", "CEO Tesla"),
         create_registration("Richard", "Fundador GNU")
     ]
-    print(participants)
+    
+    while True:
+        print_menu()
+        option = input("Seleccione una opción: ")
 
-main()
+        if option == "1":
+            for registration in participants:
+                assign_email(registration)
+        elif option == "2":
+            list_participants(participants)
+        elif option == "3":
+            print("Saliendo del sistema")
+            break
+        else: 
+            print("Opcion válida, Intente de nuevamente")
+
+
+if __name__ == "__main__":
+    main()
